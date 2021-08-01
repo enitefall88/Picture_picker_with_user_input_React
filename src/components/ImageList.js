33699187ef75c0adbaa818a334c0e0feb8371567
Anton <enitefall81@gmail.com>
2021-07-31 17:10:22 -0700

    Fixes

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 let ImageList = (props) => {
   console.log(props.images)
-   let images = props.images.map((e,i)=> {
-     return <img src={e.urls.regular}/>
+   let images = props.images.map((e)=> {
+     return <img key={e.id} src={e.urls.regular}/>
    })
   return <div>{images}</div>
 }
