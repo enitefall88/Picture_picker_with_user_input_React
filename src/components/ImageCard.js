e2bedf8319616bbf57e78da1893fffedd494f97a
Anton <enitefall81@gmail.com>
2021-08-03 10:44:45 -0700

    Add ImageCard Component for proper styling every picture

---
+++ src/components/ImageCard.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class ImageCard extends React.Component {
+  render() {
+    return <div>
+      <img
+        alt={this.props.image.description}
+        src={this.props.urls.regular}
+      />
+    </div>
+  }
+}
+
+export default ImageCard
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,8 +3,8 @@ import './ImageList.css'
 
 let ImageList = (props) => {
   console.log(props.images)
-   let images = props.images.map((e)=> {
-     return <img key={e.id} src={e.urls.regular}/>
+   let images = props.images.map(({description, id, urls})=> {
+     return <img alt={description} key={id} src={urls.regular}/>
    })
   return <div className="image-list">{images}</div>
 }
